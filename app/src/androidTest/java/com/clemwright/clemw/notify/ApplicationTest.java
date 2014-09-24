18d9e96d35b77350179da9edae31518a8de48540
Clem Wright <clemw@google.com>
2014-09-24 11:09:36 -0700

    cleaned up code for release

--- app/src/androidTest/java/com/clemwright/clemw/notify/ApplicationTest.java
+++ app/src/androidTest/java/com/clemwright/clemw/notify/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.clemw.notify;
+package com.clemwright.clemw.notify;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/com/clemwright/clemw/notify/MainActivity.java
@@ -0,0 +1,93 @@
+package com.clemwright.clemw.notify;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+    }
+
+    /*
+     * Sends a notification.
+     * Adapted from https://developer.android.com/training/wearables/notifications/creating.html
+     */
+    private void sendNotification() {
+
+        // Allows you to update the notification later on.
+        int mNotificationId = 1;
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+
+        /*
+         * The stack builder object will contain an artificial back stack for the
+         * started Activity. This ensures that navigating backward from the Activity
+         * leads out of your application to the Home screen.
+         */
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ResultActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        /*
+         * You specify the UI information and actions for a notification in a
+         * NotificationCompat.Builder object.
+         */
+        android.support.v4.app.NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification_icon)
+                .setContentTitle(getString(R.string.content_title))
+                .setContentText(getString(R.string.content_text))
+                .setPriority(2)
+                .setAutoCancel(true);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        /*
+         * To create the notification itself, you call NotificationCompat.Builder.build(),
+         * which returns a Notification object containing your specifications.
+         */
+        Notification mNotification = mBuilder.build();
+
+        // Set sound, lights, and vibrate to default
+        mNotification.defaults = Notification.DEFAULT_ALL;
+
+        /*
+         * To issue the notification, you pass the Notification object to the system by
+         * calling NotificationManager.notify()
+         */
+        mNotificationManager.notify(mNotificationId, mNotification);
+    }
+}
